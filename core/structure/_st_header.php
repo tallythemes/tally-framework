5ab8e7d2bf470ae79482d5aac7001adbe2545140
TallyThemes <wedcoder09@gmail.com>
2014-11-08 13:29:24 +0600

    adding custom header functionality

--- core/structure/_st_header.php
+++ core/structure/_st_header.php
@@ -1,7 +1,7 @@
 <?php
-function tally_st_header_menu(){
+function tally_st_header_menu($class = ''){
 	?>
-	<div class="menu_area hheight">
+	<div class="menu_area <?php echo $class; ?>">
 		<nav id="nav" class="main-navigation" role="navigation">
 			<?php wp_nav_menu( array('theme_location'=>'main_menu') ); ?>
 		</nav><!-- #site-navigation -->
@@ -9,9 +9,9 @@ function tally_st_header_menu(){
 	<?php
 }
 
-function tally_st_header_menu_alt(){
+function tally_st_header_menu_alt($class = ''){
 	?>
-	<div class="menu_alt_area hheight">
+	<div class="menu_alt_area <?php echo $class; ?>">
 		<nav id="nav_alt" role="navigation">
 			<?php wp_nav_menu( array('theme_location'=>'main_menu_alt') ); ?>
 		</nav><!-- #site-navigation -->
@@ -19,50 +19,92 @@ function tally_st_header_menu_alt(){
 	<?php
 }
 
-function tally_st_header_info(){
-	
-}
-
-function tally_st_header_phone(){
-	
-}
-
-function tally_st_header_email(){
-	
-}
-
-function tally_st_header_logo(){
+function tally_st_header_info($class = ''){
 	?>
-    <div class="logo_area hheight"><?php tallyfn_logo(tally_option('site_logo')); ?></div>
+    <div class="header-info <?php echo $class; ?>">
+    	<div class="header-info-inner">
+			<?php echo tally_option('header_info_text'); ?>
+        </div>
+    </div>
     <?php
 }
 
-function tally_st_header_social_icons(){
-	tally_social_icons($class = '');
+function tally_st_header_phone($class = '', $icon = '<i class="fa fa-phone"></i>'){
+	?>
+    <div class="header-phone <?php echo $class; ?>">
+    	<div class="header-phone-inner">
+			<?php echo $icon; ?>
+       	 <a href="tel:<?php tally_option('header_phone_number'); ?>"><?php echo tally_option('header_phone_number'); ?></a>
+        </div>
+    </div>
+    <?php
 }
 
-function tally_st_header_woocommerce_cart(){
+function tally_st_header_email($class = '', $icon = '<i class="fa fa-envelope"></i>'){
+	?>
+    <div class="header-email <?php echo $class; ?>">
+    	<div class="header-email-inner">
+			<?php echo $icon; ?>
+        	<a href="mailto:<?php tally_option('header_email_address'); ?>"><?php echo tally_option('header_email_address'); ?></a>
+        </div>
+    </div>
+    <?php
+}
+
+function tally_st_header_logo($class = ''){
+	?>
+    <div class="logo_area <?php echo $class; ?>"><?php tallyfn_logo(tally_option('site_logo')); ?></div>
+    <?php
+}
+
+function tally_st_header_social_icons($class = ''){
+	tally_social_icons($class);
+}
+
+function tally_st_header_woocommerce_cart($class = ''){
 	
 }
 
-function tally_st_header_wpml_menu(){
-	tally_wpml_language_switcher( $class = '' );
+function tally_st_header_wpml_menu($class = ''){
+	tally_wpml_language_switcher( $class);
 }
 
-function tally_st_header_advertisment(){
+function tally_st_header_advertisment($class = ''){
 	
 }
 
-function tally_st_header_serch(){
-	tally_icon_search_bar($class = '');
+function tally_st_header_serch($class = ''){
+	tally_icon_search_bar($class);
 }
 
-function tally_st_header_login(){
+function tally_st_header_login($class = '', $login = '<i class="fa fa-lock"></i> Login', $register = '<i class="fa fa-plus-square"></i> Register'){
+	?>
+    <div class="header-logins-area <?php echo $class; ?>">
+    	<div class="header-logins-inner">
+        	<?php if($login != ''){ ?><a class="header-login" href="<?php tally_option('header_login_url'); ?>"><?php echo $login; ?></a> <?php } ?>
+        	<?php if($register != ''){ ?><a class="header-register" href="<?php tally_option('header_login_url'); ?>"><?php echo $register; ?></a> <?php } ?>
+        </div>
+    </div>
+    <?php
+}
+
+function tally_get_custom_header_dri(){
+	$file_url = '';
+	$layout = tally_option('header_layout');
 	
+	if( file_exists(TALLY_CHILD_DRI . '/_st_header/' . $layout . '/' . 'header-layout.php') ){
+		$file_url = TALLY_CHILD_DRI . '/_st_header/' . $layout . '/';
+	}elseif(file_exists(TALLY_DRI . '/_st_header/' . $layout . '/' . 'header-layout.php')){
+		$file_url = TALLY_DRI . '/_st_header/' . $layout . '/';
+	}elseif(file_exists(TALLY_DRI . '/core/structure/_st_header/' . $layout . '/' . 'header-layout.php')){
+		$file_url = TALLY_DRI . '/core/structure/_st_header/' . $layout . '/';
+	}
+	
+	return $file_url;
 }
 
-$header_layout = tally_option('header_layout');
-if($header_layout != ''){
+$tally_header_layout = tally_option('header_layout');
+if($tally_header_layout != ''){
 	add_filter('tally_st_header_info', '__return_false');
 	add_filter('tally_st_header_menu_alt', '__return_false');
 	add_filter('tally_st_header_phone', '__return_false');
@@ -72,31 +114,25 @@ if($header_layout != ''){
 	add_filter('tally_st_header_wpml_menu', '__return_false');
 	add_filter('tally_st_header_advertisment', '__return_false');
 	add_filter('tally_st_header_serch', '__return_false');
-	add_filter('tally_st_header_login', '__return_false');
+	add_filter('tally_st_header_login', '__return_false');	
 	
-	$header_file_url = '';
-	
-	if( file_exists(TALLY_CHILD_DRI . '/_st_header/' . $header_layout . '/' . $header_layout . '.php') ){
-		$header_file_url = TALLY_CHILD_DRI . '/_st_header/' . $header_layout . '/';
-	}elseif(file_exists(TALLY_DRI . '/_st_header/' . $header_layout . '/' . $header_layout . '.php')){
-		$header_file_url = TALLY_DRI . '/_st_header/' . $header_layout . '/';
-	}elseif(file_exists(TALLY_DRI . '/core/structure/_st_header/' . $header_layout . '/' . $header_layout . '.php')){
-		$header_file_url = TALLY_DRI . '/core/structure/_st_header/' . $header_layout . '/';
-	}
-	
-	if(file_exists( $header_file_url )){
-		include($header_file_url.'options-filters.php');
-		add_action('tally_header', 'tally_st_custom_header_do');
-		function tally_st_custom_header_do(){
-			include($header_file_url.'header-layout.php');
+	add_action('tally_header', 'tally_st_custom_header_do');
+	function tally_st_custom_header_do(){
+		if(file_exists( tally_get_custom_header_dri() . 'header-layout.php')){
+			include(tally_get_custom_header_dri() . 'header-layout.php');
 		}
 	}
+		
+	if(file_exists( tally_get_custom_header_dri() . 'options-filters.php' )){
+		include(tally_get_custom_header_dri().'options-filters.php');
+	}
+	
 	
 }else{
 	/*
 		Header Open Div
 	--------------------------------------------*/
-	if('tally_do_header_open_warp'):
+	if('tally_do_header_open_warp'){
 		function tally_do_header_open_warp(){
 			if(tally_is_header() == 'no') return;
 			
@@ -104,14 +140,14 @@ if($header_layout != ''){
 				echo '<div id="header-inner">';
 		}
 		add_action('tally_header', 'tally_do_header_open_warp', 5);
-	endif;
+	}
 	
 	
 	
 	/*
 		Header Closing Div
 	--------------------------------------------*/
-	if('tally_do_header_closing_warp'):
+	if('tally_do_header_closing_warp'){
 		function tally_do_header_closing_warp(){
 			if(tally_is_header() == 'no') return;
 			
@@ -120,28 +156,28 @@ if($header_layout != ''){
 			echo '</div>';
 		}
 		add_action('tally_header', 'tally_do_header_closing_warp', 15);
-	endif;
+	}
 	
 	
 	
 	/*
 		Add Site Logo
 	--------------------------------------------*/
-	if('tally_do_header_logo'):
+	if('tally_do_header_logo'){
 		function tally_do_header_logo(){
 			if(tally_is_header() == 'no') return;
 			
 			?><div class="logo_area hheight"><?php tallyfn_logo(tally_option('site_logo')); ?></div><?php
 		}
 		add_action('tally_header', 'tally_do_header_logo', 10);
-	endif;
+	}
 	
 	
 	
 	/*
 		Add Main menu
 	--------------------------------------------*/
-	if('tally_do_header_menu'):
+	if('tally_do_header_menu'){
 		function tally_do_header_menu(){
 			if(tally_is_header() == 'no') return;
 			
@@ -154,5 +190,5 @@ if($header_layout != ''){
 			<?php
 		}
 		add_action('tally_header', 'tally_do_header_menu', 10);
-	endif;
+	}
 }
