db798d956a2d9ca4df6afc925e31bf4c21f37527
TallyThemes <wedcoder09@gmail.com>
2014-11-11 13:08:20 +0600

    push

---
+++ core/structure/_st_header/header_1 - Copy/header-layout.php
@@ -0,0 +1,35 @@
+<?php if(tally_is_topbar() != 'no'){ ?>
+    <div id="topbar" class="<?php echo apply_filters('tally_topbar_class', ''); ?>">
+        <div id="topbar-inner">
+            <?php tally_st_header_menu_alt('align-left vertical-align-center'); ?>
+            <?php tally_st_header_login('align-right vertical-align-center style_simple'); ?>
+            <div class="clear"></div>
+        </div>
+    </div>
+<?php } ?>
+
+<?php if(tally_is_header() != 'no'){ ?>
+    <div id="header" class="<?php echo apply_filters('tally_header_class', ''); ?> border-bottom">
+        <div id="header-inner">
+            <?php tally_st_header_logo('align-left vertical-align-center'); ?>
+            
+            <div class="align-right vertical-align-center">
+				<?php tally_st_header_phone('align-right style_simple'); ?>
+                <?php tally_st_header_email('align-right style_simple'); ?>
+                <div class="clear" style="height:5px;"></div>
+                <?php tally_st_header_social_icons('align-right style_border_round'); ?>
+            </div>
+            <div class="clear"></div>
+        </div>
+    </div>
+    
+    <div id="navigation-area" class="<?php echo apply_filters('tally_navigation_area_class', ''); ?> border-bottom">
+        <div id="navigation-area-inner">
+            <?php tally_st_header_menu('align-left vertical-align-center border_full'); ?>
+            <?php tally_st_header_woocommerce_cart('align-right vertical-align-center margin-left-10px'); ?>
+            <?php tally_st_header_wpml_menu('align-right vertical-align-center margin-left-10px'); ?>
+            <?php tally_st_header_serch('align-right vertical-align-center margin-left-10px'); ?>
+            <div class="clear"></div>
+        </div>
+    </div>
+<?php } ?>
---
+++ core/structure/_st_header/header_1 - Copy/options-filters.php
@@ -0,0 +1,11 @@
+<?php
+add_filter('tally_st_header_info', '__return_true');
+add_filter('tally_st_header_phone', '__return_true');
+add_filter('tally_st_header_email', '__return_true');
+add_filter('tally_st_header_social_icons', '__return_true');
+add_filter('tally_st_header_woocommerce_cart', '__return_true');
+add_filter('tally_st_header_wpml_menu', '__return_true');
+//add_filter('tally_st_header_advertisment', '__return_true');
+add_filter('tally_st_header_serch', '__return_true');
+add_filter('tally_st_header_login', '__return_true');
+add_filter('tally_st_header_alt_menu', '__return_true');
