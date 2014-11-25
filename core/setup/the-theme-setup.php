cd03c719d30319637fb362658af27836d3200f51
TallyThemes <wedcoder09@gmail.com>
2014-11-25 09:32:02 +0600

    Fix typo error of the admin notice

--- core/setup/the-theme-setup.php
+++ core/setup/the-theme-setup.php
@@ -1,7 +1,7 @@
 <?php
 function tally_setup_admin_notice() {
-	if( isset($_GET['page']) && ($_GET['page'] == 'tallykit_impoter-demo-importer') ) return;
-	if(get_option('tallykit_data_imported') == 'yes') return;
+	if( isset($_GET['page']) && ($_GET['page'] == 'tallykit_importer-demo-importer') ) return;
+	if(get_option('tallykit_importer_all_data_imported') == 'yes') return;
 	$theme = wp_get_theme();
     ?>
     <style type="text/css">
@@ -12,6 +12,7 @@ function tally_setup_admin_notice() {
 		margin-top:40px; 
 		border-radius: 5px; 
 		border: 1px solid #043A04;
+		margin: 10px 20px 0 2px;
 	}
 	.tally_setup_notic h2, .tally_setup_notic h3{ color:#FFF; }
 	.tally_setup_notic .button.button-hero{
@@ -23,13 +24,10 @@ function tally_setup_admin_notice() {
     	box-shadow: inset 0 1px 0 #755B98,0 1px 0 rgba(0,0,0,.08);
 	}
 	</style>
-    <div class="wrap">
-        <div class="tally_setup_notic">
-            <h2>You have success fully  installed "<strong><?php echo $theme->get( 'Name' ); ?></strong>" theme.</h2>
-            <h3>Do you want to make your site like our demo site? If yes, please click the button below.</h3>
-            <a href="<?php echo admin_url( 'themes.php?page=tallykit_impoter-demo-importer'); ?>" class="button button-hero" >Make the Site like the Demo</a>
-        
-        </div>
+    <div class="tally_setup_notic">
+		<h2>You have success fully  installed "<strong><?php echo $theme->get( 'Name' ); ?></strong>" theme.</h2>
+		<h3>Do you want to make your site like our demo site? If yes, please click the button below.</h3>
+		<a href="<?php echo admin_url( 'themes.php?page=tallykit_importer-demo-importer'); ?>" class="button button-hero" >Make the Site like the Demo</a>
     </div>
     <?php
 }
