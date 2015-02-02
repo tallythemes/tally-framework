4271c361f21d0feb4d6257c352206db2d0401cf8
TallyThemes <admin@tallythemes.com>
2015-02-02 13:08:03 +0600

    Update Font Awesome

--- core/includes/scripts-loader.php
+++ core/includes/scripts-loader.php
@@ -5,21 +5,21 @@
 add_action('wp_enqueue_scripts', 'tally_script_loader');
 function tally_script_loader(){
 	wp_enqueue_script('html5', tally_assets_file('js/html5.js'));
-	wp_enqueue_script('tinynav', tally_assets_file('js/tinynav.min.js'), array('jquery'), '', true);
-	wp_enqueue_script('jquery-flexslider', tally_assets_file('js/jquery.flexslider-min.js'),array('jquery'), '', true);
+	wp_enqueue_script('tinynav', tally_assets_file('js/tinynav.js'), array('jquery'), '', true);
+	wp_enqueue_script('jquery-flexslider', tally_assets_file('js/jquery.flexslider.js'),array('jquery'), '', true);
 	wp_enqueue_script('jquery-prettyPhoto', tally_assets_file('js/jquery.prettyPhoto.js'),array('jquery'), '', true);
-	wp_enqueue_script('jquery-easing', tally_assets_file('js/jquery.easing.min.js'), array('jquery'), '');
+	wp_enqueue_script('jquery-easing', tally_assets_file('js/jquery.easing.js'), array('jquery'), '');
 	wp_enqueue_script('jquery-wow', tally_assets_file('js/wow.min.js'), array('jquery'), '');
 	wp_enqueue_script('jquery-fitvids', tally_assets_file('js/jquery.fitvids.js'), array('jquery'), '');
 	
 	//Custom Script
 	wp_enqueue_script('theme', tally_assets_file('js/theme.js'), array('jquery'), '', true);
-	wp_enqueue_script('jquery-wow', tally_assets_file('js/wow.min.js'), array('jquery'), '', true);
+	wp_enqueue_script('jquery-wow', tally_assets_file('js/wow.js'), array('jquery'), '', true);
 	wp_enqueue_script('jquery-smoothscroll', tally_assets_file('js/smoothscroll.js'), array('jquery'), '', true);
 	
 	wp_enqueue_style('jquery-prettyPhoto', tally_assets_file('css/prettyPhoto.css'));
 	wp_enqueue_style('flexslider', tally_assets_file('css/flexslider.css'));
-	wp_enqueue_style('font-awesome', tally_assets_file('css/font-awesome.min.css'), '', ' 4.2.0');
+	wp_enqueue_style('font-awesome', tally_assets_file('css/font-awesome.css'), '', ' 4.2.0');
 	wp_enqueue_style('animate', tally_assets_file('css/animate.css'), '', '3.2.0');
 	
 	wp_enqueue_style('base', tally_assets_file2('css', 'base.css'));
